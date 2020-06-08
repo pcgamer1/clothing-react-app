4931c72101e05f2765a0fb70f54efdc4d4f25ba8
pcgamer1 <saxenasarthak72@gmail.com>
2020-06-08 21:44:29 +0530

    Bug fixes

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './collection-preview.styles.scss'
 import CollectionItem from '../collection-item/collection-item.component'
+import { Link } from 'react-router-dom'
 
 const CollectionPreview = ({title, items}) => (
     <div className='collection-preview'>
-        <h1 className='title'>{title.toUpperCase()}</h1>
+        <h1 className='title'><Link to={`shop/${title.toLowerCase()}`}>{title.toUpperCase()}</Link></h1>
         <div className='preview'>
             {
                 items
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import './menu-item.styles'
+import './menu-item.styles.scss'
 import { withRouter } from 'react-router-dom'
-import { MenuItemContainer, BackgroundImageContainer, ContentContainer, ContentTitle, ContentSubtitle } from './menu-item.styles'
 
 class MenuItem extends React.Component {
     render() {
@@ -9,13 +8,13 @@ class MenuItem extends React.Component {
         const {size, title, imageUrl, history, linkUrl, match} = this.props
 
         return (
-            <MenuItemContainer onClick={() => history.push(`${match.url}${linkUrl}`)}>
-                <BackgroundImageContainer imageUrl={imageUrl}/>
-                <ContentContainer>
-                    <ContentTitle>{title.toUpperCase()}</ContentTitle>
-                    <ContentSubtitle>SHOP NOW</ContentSubtitle>
-                </ContentContainer>
-            </MenuItemContainer>
+            <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+                <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}} />
+                <div className='content'>
+                    <h1 className='title'>{title.toUpperCase()}</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
         )
     }
 }
